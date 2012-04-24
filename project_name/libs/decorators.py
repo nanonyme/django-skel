b2933d5aed23f1762ca7d630ab95b2fa6da8ae71
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2012-04-24 17:07:30 +0300

    Restructure code in form of a class

--- project_name/libs/decorators.py
+++ project_name/libs/decorators.py
@@ -14,18 +14,26 @@ except ImportError:
 else:
     sleep_functions['gevent'] = gevent.sleep
 
-def delayed_view(interval=0.5, coroutine=None):
-    def wrapper(func):
+class DelayedView(object):
+    def __init__(self, interval=0.5, coroutine=None):
+        self.interval = interval
+        self.coroutine = coroutine
+        
+    def __call__(self, func):
         @wraps(func)
-        def inner(*args, **kwargs):
-            if coroutine is None:
-                return func.delay(*args, **kwargs).get(interval=interval)
-            sleep_function = sleep_functions.get(coroutine)
+        def wrapper(*args, **kwargs):
+            if self.coroutine is None:
+                return func.delay(*args, **kwargs).get(interval=self.interval)
+            sleep_function = sleep_functions.get(self.coroutine)
             if not sleep_function:
-                raise ValueError("Unsupported coroutine %s" % coroutine)
+                raise ValueError("Unsupported coroutine %s" % self.coroutine)
             result = func.delay(*args, **kwargs)
             while not result.ready():
-                sleep_function(interval)
+                sleep_function(self.interval)
             return result.get()
-        return inner
-    return wrapper
+        return wrapper
+
+def delayed_view(*args, **kwargs):
+    return DelayedView(*args, **kwargs)
+
+
