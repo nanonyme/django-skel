f5c49fe56bcba63daa4c9abc19aab8e7498ebb91
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2012-05-19 16:51:54 +0300

    Fix several bugs in the new structure

--- wsgi/twisted.py
+++ wsgi/twisted.py
@@ -1,4 +1,5 @@
-from wsgi import application
+from __future__ import absolute_import
+from .common import application
 from twisted.web.wsgi import WSGIResource
 from twisted.internet import reactor
 application = WSGIResource(reactor, reactor.getThreadPool(), application)
